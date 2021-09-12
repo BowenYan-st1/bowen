b546eb82d315cf966c6c7c2416479970a7f4a288
bowen <870937417@qq.com>
2021-09-12 16:55:54 +0800

    [topic]: cpp学习第一天记录 [why]: cpp学习代码记录 [how]: cpp代码编写并运行 [module]: 类，重构，引用

--- project/main.cpp
+++ project/main.cpp
@@ -1,7 +1,28 @@
-#include <stdio.h>
+#include <iostream>
 #include <string.h>
 #include "cpp_learn1.h"
 
+#if 1
+using namespace std;    //此处定义全局命令空间std，因此之后对该命名空间函数调用不需要加std::
+
+int main(int argc, char** argv)
+{
+    // using namespace std;     //也可以定义局部命令空间
+    Human ZhangShan;
+
+    ZhangShan.SetHumanName("张山");
+    ZhangShan.SetHumanAge(30);
+    ZhangShan.SetHumanJob("工程师");
+
+    ZhangShan.PrintHumanMessage();
+
+    for (int i = 0; i < argc; i++) {
+        cout << argv[i] << endl;
+    }
+	return 0;
+}
+#else
+//此处没有定义全局命名空间，因此使用该空间函数时需要加std::
 int main(int argc, char** argv)
 {
     Human ZhangShan;
@@ -13,8 +34,8 @@ int main(int argc, char** argv)
     ZhangShan.PrintHumanMessage();
 
     for (int i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
+        std::cout << argv[i] << std::endl;
     }
-    
 	return 0;
 }
+#endif  
